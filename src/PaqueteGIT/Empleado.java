038c92614c54be70e6b9a17c505b44b74077f322
arkaaiitz <arkaitz.jimenez06@somo.eus>
2024-12-16 08:33:12 +0100

    este es mi primer comentario

---
+++ src/PaqueteGIT/Empleado.java
@@ -0,0 +1,11 @@
+package PaqueteGIT;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private byte edad;
+	private float sueldo;
+	private boolean hijos; //True si tiene hijos/as false en caso contrario
+	
+}
